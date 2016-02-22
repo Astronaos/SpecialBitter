7ea3464dfaa308d58716c06490ac1f68bccd26e2
Brian W. Mulligan <bwmulligan@astro.as.utexas.edu>
2016-02-22 05:23:30 -0600

    Increase map size to 256x256

--- src/Special_Bitter.cpp
+++ src/Special_Bitter.cpp
@@ -11,11 +11,11 @@ void special_bitter_main::init(void) // initialization routine; rendering contex
 	m_dTimer = 0.0;
 	m_bFlasher_1s_50p = true;
 
-	m_ihmMap.Set_Map_Size(PAIR<unsigned int>(64,64),3);
+	m_ihmMap.Set_Map_Size(PAIR<unsigned int>(256,256),3);
 	m_ihmMap.Set_Map_Zoom(2.0);
 	for (unsigned int uiI = 0; uiI < 20; uiI++)
 		m_ihmMap.Shift_Zoom(1.0/1.1);
-	m_ihmMap.Center_Map(PAIR<int>(32,32));
+	m_ihmMap.Center_Map(PAIR<int>(128,128));
 //		printf("List init %i\n",m_ihmMap.Get_Map_List());
 
 //	Set_Draw_Mode(PROJECTION);
